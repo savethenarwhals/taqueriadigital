f134ebdad1bc4f4769fff27a54bc84e324243103
Jered Sanchez <me@jered.dev>
2021-12-08 21:56:07 -0700

    Created Console Application Project
    Created new blank console application project inside of the newly created solution.

---
+++ TDConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
