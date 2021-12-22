b14f61cd45b3696698c99551648b81932db281a6
Jered Sanchez <me@jered.dev>
2021-12-22 16:37:33 -0700

    Refactoring
    Started breaking up Main() into seperate classes and methods.

--- TDConsoleApp/Program.cs
+++ TDConsoleApp/Program.cs
@@ -6,8 +6,108 @@ namespace TDConsoleApp
     {
         static void Main(string[] args)
         {
-            TacoOrder tacoCustomer1 = new("Angelina Jolie", TacoFilling.CarneAsada, TacoSide.BlackBeans, TacoSalsa.SalsaRoja);
-            Console.WriteLine(tacoCustomer1.displayTacoOrder());
-        }        
+
+            // Need to convert this to a method that creates a new instance
+            // Ask user for fields.
+
+
+            // Console UI
+            // Add titles
+            DisplayTitleUI();
+            DisplayUserChoicesUI();
+
+
+            // Give users a menu of what you want them to choose
+
+
+
+            // You can use a do/while loop that keeps looping until
+            // the user makes a choice to quit the program.
+
+
+
+
+
+            // Certain choices will call different methods. i.e. Place an order, etc.
+            // Place order
+
+
+
+
+
+
+
+
+
+            //TacoOrder tacoCustomer1 = new("Angelina Jolie", TacoFilling.CarneAsada, TacoSide.BlackBeans, TacoSalsa.SalsaRoja);
+            //Console.WriteLine(tacoCustomer1.displayTacoOrder());
+
+
+
+
+
+
+
+        }
+
+        private static void DisplayUserChoicesUI()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Please choose from these options: ");
+            Console.WriteLine();
+            Console.WriteLine("1. Place an order");
+            Console.WriteLine("2. Show current order");
+            Console.WriteLine("3. Contact us");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Write("Choose and option number and press Enter: ");
+            string userChoice = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(userChoice);
+
+            do
+            {
+                switch (userChoice)
+                {
+                    case "1": 
+                        placeCustomerOrder();
+                        break;
+                    case "2": 
+                        showCurrentOrder();
+                        break;
+                    case "3": 
+                        Console.WriteLine("Email us at customerservice@taqueriadigital.com");
+                        break;
+                    default:
+                        Console.WriteLine("Please make a valid choices.");
+                        break;
+                }
+            } while (userChoice != "4");
+
+            Console.WriteLine("Thank you for your order!");
+        }
+
+        private static void showCurrentOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void placeCustomerOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayTitleUI()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("****** Welcome to Taqueria Digital ******");
+            Console.WriteLine("*****************************************");
+
+            Console.WriteLine();            
+        }
     }
 }
